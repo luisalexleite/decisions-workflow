709954bdb53b70258b76b6b66307f9be2a9e2d58
luisalexleite <luis.alexleite@gmail.com>
2022-01-20 17:24:54 +0000

    Criação da classe para teste

---
+++ DecisionsWorkFlow/Tests/Projects.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecisionsWorkFlow.Tests
+{
+    internal class Projects
+    {
+        public string name;
+        public string description;
+        public DateTime creationDate;
+        public int user;
+        public bool state;
+
+        public Projects(string _name, string _description, DateTime _creationDate, int _user, bool _state)
+        {
+            name = _name;
+            description = _description;
+            creationDate = _creationDate;
+            user = _user;
+            state = _state;
+        }
+    }
+}
